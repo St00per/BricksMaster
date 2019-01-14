8341adfdff08269c456fe34c4d9fb2686dbcb388
Кирилл Штеффен <kirillshteffen@gmail.com>
2019-01-14 13:03:13 +0600

    Initial Commit

---
+++ BricksMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BricksMaster
+//
+//  Created by Kirill Shteffen on 14/01/2019.
+//  Copyright © 2019 BlackBricks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
