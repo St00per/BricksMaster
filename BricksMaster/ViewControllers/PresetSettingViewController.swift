379aeb9a04ef43a5190358d5b8aa4f7d969033e3
Кирилл Штеффен <kirillshteffen@gmail.com>
2019-01-31 10:39:08 +0600

    preset settings controller code style refactor

--- BricksMaster/ViewControllers/PresetSettingViewController.swift
+++ BricksMaster/ViewControllers/PresetSettingViewController.swift
@@ -17,6 +17,31 @@ class PresetSettingViewController: UIViewController {
     var currentPreset: Preset?
     var viewShadow: UIView?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let preset = currentPreset {
+            
+        } else {
+            currentPreset = Preset(id: UUID().uuidString, name: "")
+        }
+        self.presetBricksCollectionView.register(UINib(nibName: "AddBrickPresetCell", bundle: nil), forCellWithReuseIdentifier: "AddBrickPresetCell")
+        outletsVisibilityCheck()
+        presetNameTextField.text = currentPreset?.name
+        presetNameTextField.delegate = self
+        presetNameTextField.returnKeyType = UIReturnKeyType.done
+        
+        viewShadow = UIView(frame: UIScreen.main.bounds)
+        viewShadow?.backgroundColor = UIColor.black
+        viewShadow?.alpha = 0.0
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        guard let presetFootswitchName = currentPreset?.footswitch?.name else { return }
+        footswitchButton.setTitle(presetFootswitchName, for: .normal)
+        presetBricksCollectionView.reloadData()
+    }
+    
     @IBOutlet weak var presetNameHeader: UILabel!
     
     @IBOutlet weak var presetNameTextFieldView: UIView!
@@ -25,32 +50,53 @@ class PresetSettingViewController: UIViewController {
     
     @IBOutlet weak var savePresetButton: UIButton!
     
-    @IBAction func closePresetSetting(_ sender: UIButton) {
-        //UserDevicesManager.default.userPresets.remove(at: currentPresetIndex)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
     @IBOutlet weak var footswitchButton: UIButton!
     
     @IBOutlet weak var presetSettingsView: UIView!
     
+    @IBOutlet var footswitchPicker: UIView!
+    
+    @IBOutlet var bricksPicker: UIView!
+    
+    @IBOutlet weak var presetNameTextField: UITextField!
+    
+    @IBOutlet weak var presetBricksCollectionView: UICollectionView!
+    
+    @IBOutlet weak var footswitchPickerCollectionView: UICollectionView!
+    
+    @IBOutlet weak var bricksPickerCollectionView: UICollectionView!
+    
+    @IBAction func closePresetSetting(_ sender: UIButton) {
+        //UserDevicesManager.default.userPresets.remove(at: currentPresetIndex)
+        self.dismiss(animated: true, completion: nil)
+    }
     @IBAction func showFootswitchPicker(_ sender: UIButton) {
         if let viewShadow = viewShadow {
             self.view.addSubview(viewShadow)
         }
         presetSettingsView.isUserInteractionEnabled = false
         self.view.addSubview(footswitchPicker)
-        footswitchPicker.frame = CGRect(x: 16, y: self.view.bounds.size.height, width: self.view.bounds.size.width - 32, height: 320)
+        footswitchPicker.frame = CGRect(x: 16,
+                                        y: self.view.bounds.size.height,
+                                        width: self.view.bounds.size.width - 32,
+                                        height: 320)
         UIView.animate(withDuration: 0.3, animations: {
             self.viewShadow?.alpha = 0.45
-            self.footswitchPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height - 320, width: self.view.bounds.size.width, height: 320)
-        }) { (isFinished) in
+            self.footswitchPicker.frame = CGRect(x: 0,
+                                                 y: self.view.bounds.size.height - 320,
+                                                 width: self.view.bounds.size.width,
+                                                 height: 320)
+            }) { (isFinished) in
         }
-        
+
     }
+    
     @IBAction func closeFootswitchPicker(_ sender: UIButton) {
         UIView.animate(withDuration: 0.3, animations: {
-            self.footswitchPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height, width: self.view.bounds.size.width, height: 320)
+            self.footswitchPicker.frame = CGRect(x: 0,
+                                                 y: self.view.bounds.size.height,
+                                                 width: self.view.bounds.size.width,
+                                                 height: 320)
             self.viewShadow?.alpha = 0.0
         }) { (isFinished) in
             self.presetSettingsView.isUserInteractionEnabled = true
@@ -66,17 +112,28 @@ class PresetSettingViewController: UIViewController {
         }
         presetSettingsView.isUserInteractionEnabled = false
         self.view.addSubview(bricksPicker)
-        bricksPicker.frame = CGRect(x: 16, y: self.view.bounds.size.height, width: self.view.bounds.size.width - 32, height: 500)
+        bricksPicker.frame = CGRect(x: 16,
+                                    y: self.view.bounds.size.height,
+                                    width: self.view.bounds.size.width - 32,
+                                    height: 500)
         UIView.animate(withDuration: 0.3, animations: {
             self.viewShadow?.alpha = 0.45
-            self.bricksPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height - 320, width: self.view.bounds.size.width, height: 500)
+            self.bricksPicker.frame = CGRect(x: 0,
+                                             y: self.view.bounds.size.height - 320,
+                                             width: self.view.bounds.size.width,
+                                             height: 500)
         }) { (isFinished) in
         }
     }
     
+    
+    
     @IBAction func closeBrickPicker(_ sender: UIButton) {
         UIView.animate(withDuration: 0.3, animations: {
-            self.bricksPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height, width: self.view.bounds.size.width, height: 500)
+            self.bricksPicker.frame = CGRect(x: 0,
+                                             y: self.view.bounds.size.height,
+                                             width: self.view.bounds.size.width,
+                                             height: 500)
             self.viewShadow?.alpha = 0.0
         }) { (isFinished) in
             self.presetSettingsView.isUserInteractionEnabled = true
@@ -99,42 +156,7 @@ class PresetSettingViewController: UIViewController {
         }
     }
     
-    @IBOutlet var footswitchPicker: UIView!
     
-    @IBOutlet var bricksPicker: UIView!
-    
-    
-    @IBOutlet weak var presetNameTextField: UITextField!
-    
-    @IBOutlet weak var presetBricksCollectionView: UICollectionView!
-    @IBOutlet weak var footswitchPickerCollectionView: UICollectionView!
-    @IBOutlet weak var bricksPickerCollectionView: UICollectionView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let preset = currentPreset {
-
-        } else {
-            currentPreset = Preset(id: UUID().uuidString, name: "")
-        }
-        self.presetBricksCollectionView.register(UINib(nibName: "AddBrickPresetCell", bundle: nil), forCellWithReuseIdentifier: "AddBrickPresetCell")
-        outletsVisibilityCheck()
-        presetNameTextField.text = currentPreset?.name
-        presetNameTextField.delegate = self
-        presetNameTextField.returnKeyType = UIReturnKeyType.done
-        
-        viewShadow = UIView(frame: UIScreen.main.bounds)
-        viewShadow?.backgroundColor = UIColor.black
-        viewShadow?.alpha = 0.0
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        guard let presetFootswitchName = currentPreset?.footswitch?.name else { return }
-        footswitchButton.setTitle(presetFootswitchName, for: .normal)
-        presetBricksCollectionView.reloadData()
-    }
     
     func outletsVisibilityCheck() {
         if self.currentPreset?.footswitch == nil {
@@ -158,11 +180,10 @@ class PresetSettingViewController: UIViewController {
 extension PresetSettingViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         guard let preset = self.currentPreset else {
             return 1
         }
-        
+
         let presetBricksCount = preset.presetTestBricks.count
         
         if collectionView == presetBricksCollectionView {
@@ -171,14 +192,11 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
             }
             return presetBricksCount + 1
         }
-  
-        
+
         if collectionView == footswitchPickerCollectionView {
             let footswitches = UserDevicesManager.default.userFootswitches//.filter{ !$0.new }
             return footswitches.count
         }
-        
-        
         
         if collectionView == bricksPickerCollectionView {
             return currentPreset?.footswitch?.bricks.count ?? 0
@@ -186,13 +204,11 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
         return 0
     }
     
-    
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         guard let preset = self.currentPreset else {
             return UICollectionViewCell()
         }
+        
         let presetBricksCount = preset.presetTestBricks.count
         
         if collectionView == presetBricksCollectionView {
@@ -227,8 +243,6 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
             }
         }
         
-        
-        
         if collectionView == footswitchPickerCollectionView {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FootswitchPickerCollectionViewCell", for: indexPath) as? FootswitchPickerCollectionViewCell else {
                 return UICollectionViewCell()
@@ -237,7 +251,6 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
             cell.configure(footswitch: footswitches[indexPath.row])
             return cell
         }
-        
         
         if collectionView == bricksPickerCollectionView {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BricksPickerCollectionViewCell", for: indexPath) as? BricksPickerCollectionViewCell, let bricks =  preset.footswitch?.bricks else {
@@ -250,7 +263,6 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
         return UICollectionViewCell()
     }
     
-    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView == presetBricksCollectionView {
             guard let count = currentPreset?.presetBricks.count else { return }
@@ -262,7 +274,10 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
         if collectionView == footswitchPickerCollectionView {
             currentPreset?.footswitch = UserDevicesManager.default.userFootswitches[indexPath.row]
             UIView.animate(withDuration: 0.3, animations: {
-                self.footswitchPicker.frame = CGRect(x: 0, y: self.view.bounds.size.height, width: self.view.bounds.size.width, height: 320)
+                self.footswitchPicker.frame = CGRect(x: 0,
+                                                     y: self.view.bounds.size.height,
+                                                     width: self.view.bounds.size.width,
+                                                     height: 320)
                 self.viewShadow?.alpha = 0.0
             }) { (isFinished) in
                 self.presetSettingsView.isUserInteractionEnabled = true
@@ -299,6 +314,7 @@ extension PresetSettingViewController: UICollectionViewDelegate, UICollectionVie
         }
     }
 }
+
 extension PresetSettingViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
